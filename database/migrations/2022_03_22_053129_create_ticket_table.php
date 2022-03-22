1e49052686a389776300dedbe75d5c30195401d9
verplancken <42614785+verplancken@users.noreply.github.com>
2022-03-22 00:01:04 -0600

    ticket
    cracion
    tabla ticket
    tabla fixer
    añadir mas columnas a user

---
+++ database/migrations/2022_03_22_053129_create_ticket_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('id_user')->nullable();
+            $table->foreign('id_user')
+                ->references('id')->on('users')
+                ->inDelete('set null');
+
+            $table->string('servicio_primario')->nullable();
+            $table->string('unyellow')->nullable();
+            $table->string('tint')->nullable();
+            $table->string('tint_descripcion')->nullable();
+            $table->string('klin')->nullable();
+            $table->string('protector')->nullable();
+            $table->string('factura')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket');
+    }
+};
--- database/seeders/CreateRolesHasPermissions.php
+++ database/seeders/CreateRolesHasPermissions.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use DB;
 
 class CreateRolesHasPermissions extends Seeder
 {
@@ -16,11 +17,52 @@ class CreateRolesHasPermissions extends Seeder
      */
     public function run()
     {
-        $permissions = DB::table('role_has_permissions')[
-
+        $permissions = DB::table('role_has_permissions')
+        ->create([
             'permission_id' => '1',
-            'role_id' => '1',
+            'role_id' => '1'
         ]);
 
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '2',
+            'role_id' => '1'
+        ]);
+
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '3',
+            'role_id' => '1'
+        ]);
+
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '4',
+            'role_id' => '1'
+        ]);
+
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '5',
+            'role_id' => '1'
+        ]);
+
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '6',
+            'role_id' => '1'
+        ]);
+
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '7',
+            'role_id' => '1'
+        ]);
+
+        $permissions = DB::table('role_has_permissions')
+        ->create([
+            'permission_id' => '8',
+            'role_id' => '1'
+        ]);
     }
 }
