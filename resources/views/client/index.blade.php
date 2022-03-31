cff7c868af4a006d60708b73a45259da4b6691d2
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-03-31 13:35:41 -0600

    conflicto  con js
    cambio js

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -69,10 +69,11 @@
         </div>
     </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 @endsection
 
 @section('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
 <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -284,6 +284,7 @@
     <script src="{{ asset('js/') }}" defer></script>
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
 
