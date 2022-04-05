f00b14d1ce1beb01ae196c0feb15375ef8057044
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2022-04-04 22:24:48 -0500

    Elimincacion de contendio inecesario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -213,64 +213,7 @@
                           </li>
 
                         </ul>
-{{--                  <ul class="list-unstyled ps-0">--}}
-{{--                    <li class="mb-1">--}}
-{{--                      <button class="btn align-items-center rounded button_sidebar_padre collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">--}}
-{{--                        <i class="fa fa-usd nav_icon_sidebar" aria-hidden="true" ></i>Nueva Venta--}}
-{{--                      </button>--}}
-{{--                      <div class="collapse show" id="home-collapse">--}}
-{{--                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">--}}
-{{--                          <li class="li_sidebar">--}}
-{{--                              <a href="{{ route('ticket.index') }}" class="link-dark link_sidebar rounded"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>SERVICIOS ACTIVOS</a>--}}
-{{--                          </li>--}}
-{{--                          <li class="li_sidebar">--}}
-{{--                              <a href="#" class="link-dark link_sidebar rounded"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>PROMOCIONES</a>--}}
-{{--                         </li>--}}
-{{--                          <li class="li_sidebar">--}}
-{{--                              <a href="#" class="link-dark link_sidebar rounded"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>CONTABILIDAD--}}
 
-{{--                              </a>--}}
-{{--                        </li>--}}
-{{--                        </ul>--}}
-{{--                      </div>--}}
-{{--                    </li>--}}
-{{--                    <li class="mb-1">--}}
-{{--                      <button class="btn  align-items-center rounded button_sidebar_padre collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">--}}
-{{--                        <i class="fa fa-user nav_icon_sidebar" aria-hidden="true" ></i>NUEVO CLIENTE--}}
-{{--                      </button>--}}
-{{--                      <div class="collapse show" id="dashboard-collapse">--}}
-{{--                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">--}}
-{{--                          <li class="li_sidebar"><a href="{{ route('clients.index') }}" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>CLIENTES</a></li>--}}
-{{--                          <li class="li_sidebar"><a href="#" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>RACKS</a></li>--}}
-{{--                        </ul>--}}
-{{--                      </div>--}}
-{{--                    </li>--}}
-{{--                    <li class="mb-1">--}}
-{{--                      <button class="btn  align-items-center rounded button_sidebar_padre collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">--}}
-{{--                        <i class="fa fa-desktop nav_icon_sidebar" aria-hidden="true" ></i>SUCURSAL--}}
-{{--                      </button>--}}
-{{--                      <div class="collapse show" id="orders-collapse">--}}
-{{--                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">--}}
-{{--                          <li class="li_sidebar"><a href="{{ route('products.index') }}" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>PRODUCTOS </a></li>--}}
-{{--                          <li class="li_sidebar"><a href="#" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>RECURSOS</a></li>--}}
-
-{{--                        </ul>--}}
-{{--                      </div>--}}
-{{--                    </li>--}}
-{{--                    --}}{{-- <li class="border-top my-3"></li> --}}
-{{--                    <li class="mb-1">--}}
-{{--                      <button class="btn  align-items-center rounded button_sidebar_padre collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">--}}
-{{--                        <i class="fa fa-cog nav_icon_sidebar" aria-hidden="true" ></i>CONFIGURACIÓN--}}
-{{--                      </button>--}}
-{{--                      <div class="collapse show" id="account-collapse">--}}
-{{--                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">--}}
-{{--                          <li class="li_sidebar"><a href="#" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>Profile</a></li>--}}
-{{--                          <li class="li_sidebar"><a href="#" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>Settings</a></li>--}}
-{{--                          <li class="li_sidebar"><a href="#" class="link-dark rounded link_sidebar"><i class="fa fa-arrow-right nav_icon_sidebar" aria-hidden="true"></i>Sign out</a></li>--}}
-{{--                        </ul>--}}
-{{--                      </div>--}}
-{{--                    </li>--}}
-{{--                  </ul>--}}
                 </div>
               </main>
     </div>
@@ -290,11 +233,7 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet"/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-
+    
     @include('sweetalert::alert')
 
     @yield('js')
